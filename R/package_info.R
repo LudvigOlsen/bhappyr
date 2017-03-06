a851b7b0b58204a2821dc8d0711459b254023598
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2017-03-06 13:12:25 +0100

    Initialized package documentation

---
+++ R/package_info.R
@@ -0,0 +1,8 @@
+#' bhappyr: A package for generating amusing sentences.
+#'
+#' I will add a description later...
+#'
+#' @author Ludvig Renbo Olsen, \email{r-pkgs@@ludvigolsen.dk}
+#' @docType package
+#' @name bhappyr
+NULL
